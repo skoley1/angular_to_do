45954006bcd99e2f8081af81c0be8a20cf9893e5
SUDEEP KOLEY <sudkoley@in.ibm.com>
2019-12-24 17:02:56 +0530

    first commit by sudeep

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,22 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angulartodoapp';
+export class AppComponent { 
+  todoArray=[];
+  addTodo(value)
+  {    
+    this.todoArray.push(value) ;      
+  }
+
+
+  deleteItem(todo)
+  {   
+    for(let i=0 ;i<= this.todoArray.length ;i++)
+    {    
+      if(todo== this.todoArray[i])
+      {     
+        this.todoArray.splice(i,1)    
+      }   
+    } 
+   }
 }
